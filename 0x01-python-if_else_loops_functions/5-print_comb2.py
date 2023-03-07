952f2b5d9827a099f22f0989350984450345fb1d
Momoh38 <momohabdulsalam38@gmail.com>
2023-03-07 21:37:22 +0100

    Python if and else loops

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print(i)
-else:
-    print(f"{}".format('0' + str(i) if i < 10 else i), end=", ")
+for a in range(0, 99):
+    print("{:02d}".format(a), end=", ")
+print("{:d}".format(a + 1))

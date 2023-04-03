2ce567592ff1d0958ac7a180e8fc58ce172beeeb
Momoh38 <momohabdulsalam38@gmail.com>
2023-04-03 21:40:03 +0100

    Python programming with more classes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" module that creates a rectangle and initializes its features """
+""" more practice with classes!!! """
 
 
 class Rectangle:
-    """ inidialize with the width and height with value checks
+    """ initialize with the width and height with value checks
 
     Args:
         width: how big
@@ -22,7 +22,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if isinstance(value, int) is False:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -34,8 +34,18 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if isinstance(value, int) is False:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """ calculates the area of this rekt """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ calculates the perimeter of this rect """
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
